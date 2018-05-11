6b24a47fd1f21dc3327393a00717dc998e2d0198
TheWinci <leniwyinzynier@gmail.com>
2018-05-11 13:31:08 +0200

    added printPercentage

---
+++ NODE.js/printPercentage.js
@@ -0,0 +1,20 @@
+const readLine = require("readline");
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function printPercentage(){
+    if(percentage >= 100){
+        rl.write("\nZakoczono wczytywanie.")
+        return rl.close()
+    }
+    percentage += 1;
+    readLine.clearLine(rl,0);
+    readLine(cursorTo(rl,0));
+    rl.write('Postep ${percentage}%')
+
+    setTimeout(printPercentage,100);
+}
+
+printPercentage();
